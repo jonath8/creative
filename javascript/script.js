3db1743471d0139270aedb4d176a49b835c07289
Jonas Nielsen <bobthebuilder.chfi@gmail.com>
2021-10-16 15:15:06 -0600

    API stuff

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,15 @@
 document.getElementById("gameSubmit").addEventListener("click", function(event) {
   event.preventDefault();
-  const value = document.getElementById("gameInput").name;
+  const name = document.getElementById("gameInput").value;
   if (name === "")
   return;
-  const url = "https://docs.zelda-api.apius.cc/api/" + name +"?";
+  const url = "https://zelda-api.apius.cc/api/games?name=" + name;
   fetch(url)
     .then(function(response) {
       return response.json();
     }).then(function(json) {
       let results = "";
-
-      }
+    });
 
 
   document.getElementById("gameResults").innerHTML = results;
